d2278c276872895bf52aded15bff56801d095573
Garris0n- <garrison.cgpvp@gmail.com>
2013-12-03 00:36:03 -0500

    Initial commit

---
+++ src/main/java/com/garris0n/Placeholder/Main.java
@@ -0,0 +1,21 @@
+package com.garris0n.Placeholder;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+
+
+    public void onEnable(){
+
+
+
+    }
+
+    public void onDisable(){
+
+
+
+    }
+
+}
