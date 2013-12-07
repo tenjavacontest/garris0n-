8dc87452511521c12461280d326fdcd7307353b8
Garris0n- <garrison.cgpvp@gmail.com>
2013-12-07 13:21:27 -0500

    Time to debug the gui system, then we can get into the actual plugin code :D

--- src/main/java/com/garris0n/EntityGUI/Commands/CommandEntityGUI.java
+++ src/main/java/com/garris0n/EntityGUI/Commands/CommandEntityGUI.java
@@ -1,15 +1,63 @@
 package com.garris0n.EntityGUI.Commands;
 
+import com.garris0n.EntityGUI.GUI.*;
+import com.garris0n.EntityGUI.GUI.Runnables.GUIRunnable;
+import com.garris0n.EntityGUI.Util.EZItemStack;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.*;
+import org.bukkit.entity.Player;
 
 public class CommandEntityGUI implements CommandExecutor{
 
 
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args){
 
+        if(!(commandSender instanceof Player)){
 
+            commandSender.sendMessage("This command can only be executed by players.");
+            return false;
 
-        return false;
+        }
+
+        GUIItemStack item1 = new GUIItemStack(new EZItemStack(Material.COMMAND).name(ChatColor.RESET + "Say hi"), new GUIRunnable(){
+
+            @Override
+            public void leftClick(Player player, boolean shift){
+
+                player.chat("left click, shift: " + shift);
+
+            }
+
+            @Override
+            public void rightClick(Player player, boolean shift){
+
+                player.chat("right click, shift: " + shift);
+
+            }
+
+            @Override
+            public void doubleClick(Player player){
+
+                player.chat("double click");
+
+            }
+
+            @Override
+            public void numberKeyClick(Player player, int key){
+
+                player.chat("nmber click, key: " + key);
+
+            }
+        });
+
+        GUISize size = GUISize.MEDIUM;
+        GUIItemStack[] items = new GUIItemStack[size.getSize()];
+        items[0] = item1;
+
+        new EntityGUI(size, items, "Testing Inventory").open((Player) commandSender);
+
+        return true;
 
     }
 }
