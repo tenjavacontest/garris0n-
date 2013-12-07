d678416ab53d865b66b0756a5538aafae1314269
Garris0n- <garrison.cgpvp@gmail.com>
2013-12-07 14:55:57 -0500

    I know it's testing, but that looked godawful. Now...WITNESS THE GUIPOWER OF THIS FULLY ARMED AND OPERATIONAL BUKKIT PLUGIN.

--- src/main/java/com/garris0n/EntityGUI/InterfaceHandling/InterfaceHandler.java
+++ src/main/java/com/garris0n/EntityGUI/InterfaceHandling/InterfaceHandler.java
@@ -15,7 +15,7 @@ public class InterfaceHandler{
         for(int i = 0; i < MainMenuItem.values().length; i++)
             items[i] = MainMenuItem.values()[i].getGuiItem();
 
-        new EntityGUI(size, items, ChatColor.DARK_AQUA.toString() + ChatColor.BOLD + "EntityGUI Main Menu").open(player);
+        new EntityGUI(size, items, ChatColor.BLUE.toString() + ChatColor.BOLD + "EntityGUI Main Menu").open(player);
 
     }
 
