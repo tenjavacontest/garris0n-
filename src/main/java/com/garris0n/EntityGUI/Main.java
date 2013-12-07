c0c69ff99b28354ace8b1bb650b68e389aee9559
Garris0n- <garrison.cgpvp@gmail.com>
2013-12-07 11:47:02 -0500

    command structure

--- src/main/java/com/garris0n/EntityGUI/Main.java
+++ src/main/java/com/garris0n/EntityGUI/Main.java
@@ -1,22 +1,32 @@
 package com.garris0n.EntityGUI;
 
+import com.garris0n.EntityGUI.Commands.CommandEntityGUI;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin{
 
 
+    public static Main instance;
+
+
     public void onEnable(){
 
-
+        instance = this;
+        registerCommands();
 
     }
 
     public void onDisable(){
 
-
+        instance = null;
 
     }
 
-    
+    public void registerCommands(){
+
+        getCommand("entitygui").setExecutor(new CommandEntityGUI());
+
+    }
+
 
 }
