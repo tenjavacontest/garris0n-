752c59adb86bc3e9c594fc1bd80deb5d77a12e62
Garris0n- <garrison.cgpvp@gmail.com>
2013-12-07 11:37:53 -0500

    i dunno how 2 gits

---
+++ src/main/java/com/garris0n/EntityGUI/Main.java
@@ -0,0 +1,22 @@
+package com.garris0n.EntityGUI;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+
+
+    public void onEnable(){
+
+
+
+    }
+
+    public void onDisable(){
+
+
+
+    }
+
+    
+
+}
