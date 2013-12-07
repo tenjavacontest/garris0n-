433ba1be89c8cd1c4e5b28db402cc0ce8936fd74
Garris0n- <garrison.cgpvp@gmail.com>
2013-12-07 11:48:26 -0500

    more command structure...I suck at git

---
+++ src/main/java/com/garris0n/EntityGUI/Commands/CommandEntityGUI.java
@@ -0,0 +1,15 @@
+package com.garris0n.EntityGUI.Commands;
+
+import org.bukkit.command.*;
+
+public class CommandEntityGUI implements CommandExecutor{
+
+
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args){
+
+
+
+        return false;
+
+    }
+}
